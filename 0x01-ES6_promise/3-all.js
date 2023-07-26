4370ead9fcf0652434130aab0e208ceff3e87a89
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-07-26 04:12:23 +0100

    Handle error in multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,11 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    console.log(
-      `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
-    );
-  });
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(
+        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
+      );
+    })
+    .catch(console.log('Signup system offline'));
 }
