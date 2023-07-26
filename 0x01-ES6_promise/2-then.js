99e6bd1e770fce159ab0787ed2ac964c8a2a0275
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-07-26 03:26:43 +0100

    Handle promised response
    Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
