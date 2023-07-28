79a6d21364de409e0c2af880d9d70fb3f04834ec
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-07-28 02:00:20 +0100

    Use Symbol.toPrimitive

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    switch (hint) {
+      case 'number':
+        return this.size;
+      case 'string':
+        return this.location;
+      default:
+        return `HolbertonClass ${this.location}`;
+    }
+  }
+}
