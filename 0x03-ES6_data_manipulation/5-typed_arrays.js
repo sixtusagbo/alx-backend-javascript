a05e731648c710aff6111d2b7fc3e224015f7315
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-02 01:05:02 +0100

    Change data view to Uint8Array instead

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, number) {
   const buffer = new ArrayBuffer(length);
-  const dataView = new Int8Array(buffer);
+  const dataView = new Uint8Array(buffer);
   try {
     dataView[position] = number;
   } catch (error) {
