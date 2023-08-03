285c807396bd430a68a9ba27eced6fca03ea26c9
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-03 03:55:16 +0100

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,26 @@ interface printTeacherFunction {
 function printTeacher(firstName: string, lastName: string) {
   return `${firstName.slice(0, 1)}. ${lastName}`;
 }
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass
+  implements StudentInterface
+{
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+};
