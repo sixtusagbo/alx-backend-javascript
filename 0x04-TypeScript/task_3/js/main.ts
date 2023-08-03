33ed22723ec2ae1118e4cfbef9220d7c3ff445cf
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-03 05:16:07 +0100

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,20 @@
+/// <reference path="./crud.d.ts" />
+import * as CRUD from './crud.js';
+import { RowElement, RowID } from './interface';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  age: 23,
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+
+CRUD.deleteRow(newRowID);
