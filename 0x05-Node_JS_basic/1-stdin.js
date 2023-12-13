61e878001ec9c893c48f596cbb441f88c6a4f02c
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-12-13 00:54:12 +0000

    Use arrow functions instead

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,13 +3,13 @@
 process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', function () {
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
