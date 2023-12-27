1f765fc7e769409c6808b70743be963805c85a51
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-12-27 11:27:08 +0100

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const countStudents = (dbFilePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+      if (err) throw new Error('Cannot load the database');
+
+      // Split the data into array of lines
+      const lines = data
+        .split('\n')
+        .map((line) => line.trim())
+        .filter((line) => line);
+
+      // Covert each line into array of values
+      const rows = lines.slice(1).map((line) => line.split(','));
+
+      console.log(`Number of students: ${rows.length}`);
+      const fieldIndex = 3;
+      const firstNameIndex = 0;
+      const fieldValues = rows.map((row) => row[fieldIndex]);
+      const fields = new Set(fieldValues);
+      for (const field of fields) {
+        const students = [];
+
+        for (const row of rows) {
+          if (row[fieldIndex] === field) {
+            students.push(row[firstNameIndex]);
+          }
+        }
+
+        const firstNames = students.join(', ');
+        console.log(
+          `Number of students in ${field}: ${students.length}. List: ${firstNames}`,
+        );
+      }
+
+      resolve();
+    });
+  });
+};
+
+module.exports = countStudents;
