82bb50a9680777679527831d9f6842b74dbc13a1
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-12-27 09:17:12 +0100

    Reading a file synchronously with Node JS01~

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const countStudents = (dbName) => {
+  fs.readFile(dbName, 'utf8', (err, data) => {
+    if (err) throw Error('Cannot load the database');
+
+    // Split the data into array of lines
+    const lines = data
+      .split('\n')
+      .map((line) => line.trim())
+      .filter((line) => line);
+
+    // Covert each line into array of values
+    const rows = lines.slice(1).map((line) => line.split(','));
+
+    console.log(`Number of students: ${rows.length}`);
+    const fieldIndex = 3;
+    const firstNameIndex = 0;
+    const fieldValues = rows.map((row) => row[fieldIndex]);
+    const fields = new Set(fieldValues);
+    for (const field of fields) {
+      const students = [];
+
+      for (const row of rows) {
+        if (row[fieldIndex] === field) {
+          students.push(row[firstNameIndex]);
+        }
+      }
+
+      const firstNames = students.join(', ');
+      console.log(
+        `Number of students in FIELD: ${students.length}. List: ${firstNames}`
+      );
+    }
+  });
+};
+
+module.exports = countStudents;
