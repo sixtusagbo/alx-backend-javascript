6627bd8395789a72d9202d29567dcdbcf9a8ebbd
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-09-19 01:36:51 +0000

    Fix lint issues

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,45 +1,42 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const countStudents = (path) =>
-  new Promise((resolve, reject) =>
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        return reject(new Error('Cannot load the database'));
+const countStudents = (path) => new Promise((resolve, reject) => fs.readFile(path, 'utf-8', (err, data) => {
+  if (err) {
+    return reject(new Error('Cannot load the database'));
+  }
+
+  // Split data into array of lines
+  const lines = data
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => line);
+
+  // Convert each line into array of values
+  const rows = lines.slice(1).map((line) => line.split(','));
+
+  // Work with the database
+  console.log(`Number of students: ${rows.length}`);
+  const fieldIndex = 3;
+  const firstNameIndex = 0;
+  const fieldValues = rows.map((row) => row[fieldIndex]);
+  const fields = new Set(fieldValues);
+  for (const field of fields) {
+    const students = [];
+
+    for (const row of rows) {
+      if (row[fieldIndex] === field) {
+        students.push(row[firstNameIndex]);
       }
+    }
 
-      // Split data into array of lines
-      const lines = data
-        .split('\n')
-        .map((line) => line.trim())
-        .filter((line) => line);
+    const firstNames = students.join(', ');
+    let msg = `Number of students in ${field}: ${students.length}. `;
+    msg += `List: ${firstNames}`;
+    console.log(msg);
+  }
 
-      // Convert each line into array of values
-      const rows = lines.slice(1).map((line) => line.split(','));
-
-      // Work with the database
-      console.log(`Number of students: ${rows.length}`);
-      const fieldIndex = 3;
-      const firstNameIndex = 0;
-      const fieldValues = rows.map((row) => row[fieldIndex]);
-      const fields = new Set(fieldValues);
-      for (const field of fields) {
-        const students = [];
-
-        for (const row of rows) {
-          if (row[fieldIndex] === field) {
-            students.push(row[firstNameIndex]);
-          }
-        }
-
-        const firstNames = students.join(', ');
-        let msg = `Number of students in ${field}: ${students.length}. `;
-        msg += `List: ${firstNames}`;
-        console.log(msg);
-      }
-
-      return resolve();
-    }),
-  );
+  return resolve();
+}));
 
 module.exports = countStudents;
