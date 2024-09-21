d2a9899340337d7a15aa83590845e827d08fae76
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-09-21 16:38:13 +0000

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function () {
+  it('should return the correct sum of 2 integers', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return the correct sum of 2 floats', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should return the correct sum of 2 negative integers', function () {
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+  });
+  it('should return the correct sum of 2 negative floats', function () {
+    assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
+  });
+  it('should return the correct sum of an integer and a float', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return the correct sum of a float and an integer', function () {
+    assert.strictEqual(calculateNumber(1.5, 3), 5);
+  });
+  it('should return the correct sum of 2 zeros', function () {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+  it('should return the correct sum of an integer and a string', function () {
+    assert.strictEqual(calculateNumber(1, '3'), 4);
+  });
+  it('should return the correct sum of a string and an integer', function () {
+    assert.strictEqual(calculateNumber('1', 3), 4);
+  });
+  it('should return the correct sum of 2 strings', function () {
+    assert.strictEqual(calculateNumber('1', '3'), 4);
+  });
+});
+
