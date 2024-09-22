14ae162884327b7a97f2fe39038297cb1deb2072
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-09-22 06:49:00 +0000

    Use stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call calculateNumber with the correct arguments', function () {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
